a09fb568a91cc85a77a32c47985f2522071a0ea4
Tor is Here <kittikorn.a@gmail.com>
2016-06-15 01:34:11 +0700

    Test

--- SwiftyProgressBar/CircleView.swift
+++ SwiftyProgressBar/CircleView.swift
@@ -48,7 +48,7 @@ extension CGFloat {
     }
     
     required public init(coder aDecoder: NSCoder) {
-        
+        super.init(coder: aDecoder)!
     }
     
 }
--- SwiftyProgressBar/ProgressView.swift
+++ SwiftyProgressBar/ProgressView.swift
@@ -36,7 +36,7 @@ import UIKit
     
     
     required public init(coder aDecoder: NSCoder) {
-    
+        super.init(coder: aDecoder)
     }
     
     public func animate(toProgress:CGFloat, duration:CFAbsoluteTime) {
