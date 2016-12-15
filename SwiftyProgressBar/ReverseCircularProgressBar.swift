ce24c5fdf29b9fbcf108bca43986962eb89a2aa7
Kittikorn Ariyasuk <kittikorn.ari@ascendcorp.com>
2016-12-15 09:31:41 +0700

    update travis

---
+++ SwiftyProgressBar/ReverseCircularProgressBar.swift
@@ -0,0 +1,13 @@
+//
+//  ReverseCircularProgressBar.swift
+//  SwiftyProgressBar
+//
+//  Created by apple on 12/15/2559 BE.
+//  Copyright © 2559 TorIsHere. All rights reserved.
+//
+
+import UIKit
+
+class ReverseCircularProgressBar: NSObject {
+
+}
